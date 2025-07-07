419c19bfb6e50ca3bdf24c0d4c6f8fa7be8cf586
Luis Flores Álvarez <luis.flores.alvarez@icloud.com>
2025-07-06 20:19:11 -0600

    feat(foundations): callbacks

--- 02-foundations/src/app.js
+++ 02-foundations/src/app.js
@@ -1,4 +1,15 @@
+console.log('01-template.js');
 const { emailTemplate } = require('./js-foundations/01-template.js');
+console.log(emailTemplate);
+
+console.log('02-destructuring.js');
 require('./js-foundations/02-destructuring.js');
 
-console.log(emailTemplate);
+console.log('03-callbacks.js');
+const { getUserById } = require('./js-foundations/03-callbacks.js');
+getUserById(3, function (error, user) {
+  if (error) {
+    throw new Error(error);
+  }
+  console.log(user);
+});
